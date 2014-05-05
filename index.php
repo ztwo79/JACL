7c0e376a16e8d0f451871c3c370654bcd98f0395
ztwo79 <ztwo79@gmail.com>
2014-05-05 10:40:15 +0800

    add lib folder and change bootstrap location

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>INDEX</title>
 
     <!-- Bootstrap -->
-    <link href="include/bootstrap-3.1.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="lib/bootstrap-3.1.1/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -35,7 +35,7 @@
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="js/jquery-1.11.0.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="include/bootstrap-3.1.1//js/bootstrap.min.js"></script>
+    <script src="lib/bootstrap-3.1.1//js/bootstrap.min.js"></script>
     <div class="container">
       
         <div class="row">
