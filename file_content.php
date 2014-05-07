1bdf3ee12c78923d362d088782d5475e38867d48
ztwo79 <ztwo79@gmail.com>
2014-05-07 18:35:41 +0800

    add log file reader

--- file_content.php
+++ file_content.php
@@ -18,7 +18,12 @@ if ($_POST) {
 	}
 }
 
-// $table_name="agents_metaphor";		// 之後接收GET就刪掉這行
+
+//echo "$log_file_name";	// $log_file_name 接收的log檔名稱
+
+//$table_name="agents_metaphor";		// 之後接收GET就刪掉這行
+
+if(isset($table_name)){
 
 
 // 使用 information_schema.columns 資料表找出某一資料庫的某一資料表的欄位資訊
@@ -29,12 +34,15 @@ $sql_colname = "SELECT * FROM  information_schema.columns where TABLE_SCHEMA='ac
 				$colname_rs=mysql_num_rows($colname_exe);	// $colname_rs 有幾筆資料
 				
 
-echo '<TABLE BORDER="1" ALIGN="center">';	// 輸出表格開始
+// <table style="font-size:像素或點數;color:顏色代碼;background-color:顏色代碼">
+
+
+echo '<TABLE BORDER="1" ALIGN="center" style="font-size:9px">';	// 輸出表格開始
 echo '<tr bgcolor="#859e78">';	// 第一行(顯示欄位名稱)，設欄位顏色
 echo '<td></td>';		// 第一行第一格空格
 				
 // $colname_exe 每筆資料做輸出
-for($i=0; $i < $colname_rs ;$i++)
+for($i=1; $i < $colname_rs ;$i++)
 {
 		$rowcolname = mysql_fetch_assoc($colname_exe);	// $rowcolname 是$colname_exe的一筆資料
 		
@@ -42,9 +50,8 @@ for($i=0; $i < $colname_rs ;$i++)
 		
 }
 				
+				
 echo '</tr>';		// 第一行結束
-
-
 
 
 
@@ -58,25 +65,138 @@ $sql_alldata = "SELECT * FROM $table_name";
 
 	
 // $alldata_exe 每筆資料做輸出
-for($i=1; $i <= $alldatanumber ;$i++)
-{
+for($i=1; $i <= $alldatanumber ;$i++) {
 	echo '<tr>';	//一行
 	echo '<td bgcolor="#859e78">'.$i.'</td>';	// 每行第一格顯示數字(第幾筆資料)，欄位顏色
 	
 	$onelinedata = mysql_fetch_row($alldata_exe);	// $onelinedata 是 $alldata_exe 的一筆資料
 	
+	$colname_rsdownone = $colname_rs - 1;	// 最後一欄不用輸出
+	
 	//用上一個sql找出的欄位個數($colname_rs)，輸出這筆資料($onelinedata)的每一格資料
-	for($j=0; $j < $colname_rs ;$j++)
+	for($j=0; $j < $colname_rsdownone ;$j++)
 	{
+		
+		//$onelinedata2 = $onelinedata[$j];
+		//$nochangeline = str_replace ("\n","\t",$onelinedata2);
+		
+		
+		
 		echo '<td bgcolor="#f2f2c2">';	//一格，欄位顏色
+		
+		//echo $nochangeline;	// $onelinedata[$j] 顯示 $onelinedata 的第j格資料
 		echo $onelinedata[$j];	// $onelinedata[$j] 顯示 $onelinedata 的第j格資料
 		echo '</td>';
 	}
+	
+	
 	
 	echo '</tr>';
 		
 }
 
 echo '</TABLE>';	//表格結束
+
+	mysql_free_result($colname_exe);
+	mysql_free_result($alldata_exe);
+
+
+}
+
+// 取得lOG 檔
+if(isset($log_file_name)){
+
+	$LOG_project_file="$log_file_name.LOG";
+	$LOG_project_src="ACL DATA/$LOG_project_file";
+
+	$LOG_project_file_fp=fopen($LOG_project_src , "r");
+	$all_size=filesize($LOG_project_src);
+	$all=fread($LOG_project_file_fp, $all_size);
+	$all=mb_convert_encoding($all, 'UTF-8', 'UTF-16LE');
+
+	//echo "$LOG_project_src";
+	//echo "$all";
+
+	$nowline = 0;
+
+	$data_arr=explode("\n", $all);	// $data_arr : LOG檔分成一行一行
+
+	if (!empty($data_arr)) {
+
+		//echo "enter if";
+
+
+
+
+		//每行找
+		foreach ($data_arr as $line => $each_line) {
+			$nowline = $nowline +1;	//記錄log檔目前行數
+			//echo "enter foreach";
+			$LOG_big_chk = strpos($each_line,"@@");
+			if ($LOG_big_chk!==false) {
+				//echo "enter m if";
+				$LOG_big_arr=explode(" Opened at ", $each_line);	//用Opened at分前後句
+				//$LOG_big_arr = array_diff($LOG_arr, array(null,'null','',' '));	//??在這是否需要
+				$LOG_big_time_all = next($LOG_big_arr);	//$LOG_big_time_all : Opened at 的後半段字
+
+				$LOG_big_time_arr = explode(" ", $LOG_big_time_all);	//$LOG_big_time_arr :Opened at 後半段字分成陣列
+				$LOG_big_time_time = next($LOG_big_time_arr);	//$LOG_big_time_time :時間
+				$LOG_big_time_on = next($LOG_big_time_arr);		//$LOG_big_time_on :on
+				$LOG_big_time_day = next($LOG_big_time_arr);	//$LOG_big_time_day :日期
+
+				echo "$LOG_big_time_time ";
+				echo "$LOG_big_time_on ";
+				echo "$LOG_big_time_day";
+				echo "<br>";
+				$LOG_second_chk=true;
+				//$LOG_second_arr = array();
+
+				continue;
+			}	
+
+			if($LOG_second_chk == true){
+				$LOG_detail_oneline_chk = strpos($each_line,"@ OPEN");
+				if($LOG_detail_oneline_chk!==false){
+					//$detail_line_number = 0;
+					$LOG_second_arr=explode(" ", $each_line);
+					$LOG_second_open = next($LOG_second_arr);
+					$LOG_second_tablename = next($LOG_second_arr);
+
+					$LOG_detail_chk = true;
+					$LOG_second_chk = false;
+
+					echo "--";
+					echo "$LOG_second_tablename";
+					$LOG_detal_line = 1;
+				}
+				continue;
+			}
+
+			if($LOG_detail_chk == true){
+				//echo "enter detail";
+				//$LOG_detal_line = $LOG_detal_line + 1;
+				$LOG_detail_oneword_chk = strpos($each_line,"fields activated");
+				if($LOG_detail_oneword_chk!==false){
+
+					echo "    yes";
+					echo "<br>";
+					//if($LOG_detail_line ==3){
+						$LOG_second_chk = true;
+					//}
+				}
+
+				$LOG_detail_oneword_chk = strpos($each_line,"Command Failed");
+				if($LOG_detail_oneword_chk!==false){
+					echo "    no";
+					echo "<br>";
+					//if($LOG_detail_line ==4){
+						$LOG_second_chk = true;
+					//}
+				}
+				continue;
+			}
+		}
+	}
+}
 
 ?>
